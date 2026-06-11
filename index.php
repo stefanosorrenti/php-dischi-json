6652b53c14b2178a12764b6bf9f32bed10a643d7
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-06-11 22:17:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I miei dischi</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <header>
+        <h1 class="text-danger">Prova</h1>
+    </header>
+</body>
+</html>
