836bfb8cbdc1a4ca1cddc92d4f0a9c8c13c9dcd8
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-06-11 22:38:43 +0200

    add html structure

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +8,46 @@
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </head>
+
 <body>
+
+    <!-- Header -->
     <header>
-        <h1 class="text-danger">Prova</h1>
+        <nav class="navbar bg-body-tertiary p-3">
+            <div class="container-fluid">
+                <a class="nav-link" href="index.php">I MIEI DISCHI</a>
+            </div>
+        </nav>
+
+        <!-- Main -->
+        <main class="container">
+
+            <!-- Card Section -->
+            <section class="row gy-3">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="card">
+
+                        <img src="" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Titolo disco</h5>
+                            <p class="card-text">
+                            <ul>
+                                <li>Artista</li>
+                                <li>Anno</li>
+                                <li>Genere</li>
+                            </ul>
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                </div>
+            </section>
+            </div>
+        </main>
+
+        <!-- titolo, artista, url della cover, anno di pubblicazione, genere -->
     </header>
 </body>
+
 </html>
